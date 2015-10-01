ec0e09fe47f632286995722d536a7a8f08707fdb
sergio-daniels <daniels.sergio@gmail.com>
2015-10-01 15:16:59 +0200

    init

---
+++ http.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  res.write('Hallo Welt');
+  res.end();
+}).listen(3000);
